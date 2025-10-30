40ab39dccf0e457037bf72a3167910742f1dd97a
vito-filo <filomeno.vito@gmail.com>
2025-10-30 17:16:33 +0100

    chore: add debug logs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,9 @@ const globalForPrisma = global as unknown as {
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
 
+console.log("Node Env:", process.env.NODE_ENV);
+console.log("Database URL:", process.env.DATABASE_URL);
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
