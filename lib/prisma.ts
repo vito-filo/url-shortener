8eeae9b79712948c889e2e90ec36a83d9c1864da
vito-filo <filomeno.vito@gmail.com>
2025-10-30 19:43:05 +0100

    fix: change URL

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,6 +8,7 @@ const prisma = globalForPrisma.prisma || new PrismaClient();
 
 console.log("Node Env:", process.env.NODE_ENV);
 console.log("Database URL:", process.env.DATABASE_URL);
+console.log("Postgres Prisma URL:", process.env.POSTGRES_PRISMA_URL);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
