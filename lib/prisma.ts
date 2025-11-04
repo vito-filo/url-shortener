6144b121189056f405b078d5ad300ffff3c63878
vito-filo <filomeno.vito@gmail.com>
2025-11-04 12:32:37 +0100

    fix: remove sql engine dependency

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,7 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../app/generated/prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
-
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-console.log("Node Env:", process.env.NODE_ENV);
-console.log("Database URL:", process.env.DATABASE_URL);
-console.log("Postgres Prisma URL:", process.env.POSTGRES_PRISMA_URL);
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
