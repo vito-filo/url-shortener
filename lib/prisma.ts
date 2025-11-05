628861d23a6ed4450a4f7b06feac91748b0bf68c
vito-filo <filomeno.vito@gmail.com>
2025-11-05 08:38:09 +0100

    fix: check CA cert in prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,18 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../app/generated/prisma/client";
+import pg from "pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const ca = process.env.DATABASE_CA_CERT;
+
+const pool = new pg.Pool({
+  connectionString: process.env.POSTGRES_PRISMA_URL!,
+  ssl: {
+    rejectUnauthorized: true,
+    ca,
+  },
+});
+
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
