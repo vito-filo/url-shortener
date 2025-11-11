22475f87e5c49ab2d48a2de72bbd0b4bb8da3f52
vito-filo <filomeno.vito@gmail.com>
2025-11-11 08:55:18 +0100

    fix: create singleton client pool

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,13 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../app/generated/prisma/client";
 import pg from "pg";
 
-const ca = process.env.DATABASE_CA_CERT;
+const ca = Buffer.from(process.env.DATABASE_CA_CERT || "", "base64").toString(
+  "utf-8"
+);
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
 const pool = new pg.Pool({
   connectionString: process.env.PRISMA_CUSTOM_URL!,
@@ -11,8 +17,10 @@ const pool = new pg.Pool({
     ca,
   },
 });
-
 const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+// Prevent multiple instances of Prisma Client in development
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
